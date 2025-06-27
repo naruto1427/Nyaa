faed835fadce6b604f5fe0340490c4604dad7f5b
naruto1427 <pritnaruto@gmail.com>
2025-06-27 21:39:34 +0530

    Update nyaa_bot.py

--- nyaa_bot.py
+++ nyaa_bot.py
@@ -185,4 +185,10 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
